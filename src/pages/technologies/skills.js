9f2bf96ed8e35d1a5ea6ec38a74d46d1bc0c2691
Shidhartha <sidroy192@gmail.com>
2023-07-14 20:25:56 +0530

    Initial Carousel Slider

--- src/pages/technologies/skills.js
+++ src/pages/technologies/skills.js
@@ -1,11 +1,11 @@
 import PageHeader from "../../components/PageHeader";
 import "../technologies/technologies.css"
-
+import Slider from "../../components/skill";
 const Technologies = () =>{
     return(
         <section className="tech" id="tech">
             <PageHeader title="Technologies" description="I know these" />
-            
+            <Slider />
         </section>
     )
 }
