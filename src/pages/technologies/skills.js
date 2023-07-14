59f98c72e706cb448c00526a4482c3311c2e950c
Shidhartha <sidroy192@gmail.com>
2023-07-14 14:50:34 +0530

    technologies merge commit

---
+++ src/pages/technologies/skills.js
@@ -0,0 +1,13 @@
+import PageHeader from "../../components/PageHeader";
+import "../technologies/technologies.css"
+
+const Technologies = () =>{
+    return(
+        <section className="tech" id="tech">
+            <PageHeader title="Technologies" description="I know these" />
+            
+        </section>
+    )
+}
+
+export default Technologies
